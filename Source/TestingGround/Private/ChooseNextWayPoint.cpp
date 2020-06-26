51d2f9e23af9b312bb62a4c5553c9e37942f208b
Sishir512 <sishir.wagle512@gmail.com>
2020-06-26 14:09:24 +0545

    In this section we have done some chnages to the project Build file . AIModule , GameplayTasks have been added in PublicDependencyModuleNames inside the build file . Alse we have created a new c++ file that inherits from BTTaskNode and we have overridden ExecuteTask.

---
+++ Source/TestingGround/Private/ChooseNextWayPoint.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChooseNextWayPoint.h"
+
+EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+	UE_LOG(LogTemp, Warning, TEXT("Donkey ChooseNextWayPoint running"));
+	return EBTNodeResult::Succeeded;
+}
