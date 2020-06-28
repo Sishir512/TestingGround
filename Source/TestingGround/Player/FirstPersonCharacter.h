04a6d483a387db331d647e24880e7697310b349e
Sishir512 <sishir.wagle512@gmail.com>
2020-06-28 13:59:04 +0545

    First section of refactoring of c++ files .

--- Source/TestingGround/Player/FirstPersonCharacter.h
+++ Source/TestingGround/Player/FirstPersonCharacter.h
@@ -4,12 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "TestingGroundCharacter.generated.h"
+#include "FirstPersonCharacter.generated.h"
 
 class UInputComponent;
 
 UCLASS(config=Game)
-class ATestingGroundCharacter : public ACharacter
+class AFirstPersonCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
@@ -46,7 +46,7 @@ class ATestingGroundCharacter : public ACharacter
 	class UMotionControllerComponent* L_MotionController;
 
 public:
-	ATestingGroundCharacter();
+	AFirstPersonCharacter();
 
 protected:
 	virtual void BeginPlay();
